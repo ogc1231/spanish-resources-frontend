5edf4b07e7616f47cad6c2561142f31a844efa6b
ogc1231 <ogc@live.ie>
2023-09-05 08:30:19 +0000

    Add PostPage and likes & feed routes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,26 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
 
   return (
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Main}>
             <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              <Route exact path="/" render={() => <PostPage message="No results found, Adjust search keyword" />} />
+              <Route exact path="/feed" render={() => <PostPage 
+              message="No results found, Adjust search keyword or follow a user" 
+              filter={`owner__followed__owner__profile=${profile_id}&`}/>} 
+            />
+              <Route exact path="/liked" render={() => <PostPage 
+              message="No results found, Adjust search keyword or like a post" 
+              filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}/>} 
+            />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
